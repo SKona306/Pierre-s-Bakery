1c307cc759c56d7ef0b6344803c72d1ce6fa4bdb
Shaun Kent <shaunkent81@gmail.com>
2021-09-11 19:29:45 -0700

    add price determine method for Pastry class and code to pass test

--- PierresBakery/Models/Bakery.cs
+++ PierresBakery/Models/Bakery.cs
@@ -39,5 +39,20 @@ namespace PierresBakery.Models
       Price = price;
       Amount = amount;
     }
+
+    public int DeterminePrice()
+    {
+      if(Amount > 2)
+      {
+        int remainder = Amount % 3;
+        Price = (((Amount - remainder) / 3) * 5) + (remainder * 2);
+        return Price;
+      }
+      else
+      {
+        Price = Amount * 2;
+        return Price;
+      }
+    }
   }
 }
