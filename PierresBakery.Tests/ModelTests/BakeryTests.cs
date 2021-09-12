741736408a6a9c74683836ab8e4cdfff7f9688c0
Shaun Kent <shaunkent81@gmail.com>
2021-09-11 17:59:43 -0700

    add inital setup of MSTest and add first test

---
+++ PierresBakery.Tests/ModelTests/BakeryTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
+
+namespace PierresBakery.TestTools
+{
+  [TestClass]
+  public class BakeryTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread(1);
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+  }
+}
