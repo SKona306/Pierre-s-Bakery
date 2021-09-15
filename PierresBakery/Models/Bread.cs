0f65be741b4450c5d7053fe92405aae987a85e46
Shaun Kent <shaunkent81@gmail.com>
2021-09-15 16:18:14 -0700

    split logic for bread and pastry to seperate cs files

---
+++ PierresBakery/Models/Bread.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace PierresBakery.Models
+{
+  public class Bread
+  {
+    public int Price {get; set;}
+    public int Amount {get; set;}
+
+    public Bread(int amount, int price = 0)
+    {
+      Price = price;
+      Amount = amount;
+    }
+
+    public int DeterminePrice()
+    {
+      if(Amount > 2) 
+      {
+        int remainder = Amount % 3;
+        Price = (((Amount - remainder) / 3) * 10) + (remainder * 5);
+        return Price;
+      }
+      else 
+      {
+        Price = Amount * 5;
+        return Price;
+      }
+    }
+  }
+}
